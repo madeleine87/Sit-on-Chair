ee7e40a5a81386d4ba36167c0dcbf8e4c06f1b36
Magdalena Włodek <magdalenawlodek87@gmail.com>
2017-02-03 17:13:51 +0100

    Slider

--- js/style.js
+++ js/style.js
@@ -1,5 +1,34 @@
 document.addEventListener("DOMContentLoaded", function () {
-
+    
+   /* Slider */ 
+    var next = document.querySelector('#nextPicture');
+    var prev = document.querySelector('#prevPicture'); 
+    var li = document.querySelectorAll('.slider li');
+    var index = 0;
+    
+    li[index].classList.add('visible');
+    
+    next.addEventListener('click', function(){
+        li[index].classList.remove('visible');
+        index++;
+        if(index > li.length - 1){
+            index = 0;
+        }
+        li[index].classList.add('visible');
+    });
+    
+    prev.addEventListener('click', function(){
+        li[index].classList.remove('visible');
+        index--;
+        if(index < 0) {
+            index = li.length-1;;
+        }
+        li[index].classList.add('visible');
+    });
+    
+    
+/* Znikające divy na obrazku */
+    
     var image = document.querySelectorAll(".dis_child");
     for (i = 0; i < image.length; i++) {
         image[i].addEventListener("mouseover", function (event) {
@@ -13,4 +42,7 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
+    
+    
+    
 });
